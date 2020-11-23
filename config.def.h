1f9a72343ec5cdeefe91d8d8afffa653c54fd397
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2020-11-23 18:58:31 +0800

    Revert back tabwidth to 200

--- config.def.h
+++ config.def.h
@@ -11,7 +11,7 @@ static const char* urgfgcolor   = "#ff0000";
 static const char before[]      = "";
 static const char after[]       = "";
 static const char titletrim[]   = "...";
-static const int  tabwidth      = 100;
+static const int  tabwidth      = 200;
 static const Bool foreground    = True;
 static       Bool urgentswitch  = False;
 
